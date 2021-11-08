6c41cc046618f00d86fcd4759c2d9ab620a95134
tdxa <anndoman3@gmail.com>
2021-11-08 21:56:50 +0100

    Reformat file

--- assigment_06.py
+++ assigment_06.py
@@ -1,6 +1,6 @@
-def combine_lists(array1: list, array2:list) -> list:
-    return [number**3 for number  in list(set(array1+array2))]
+def combine_lists(array1: list, array2: list) -> list:
+    return [number ** 3 for number in list(set(array1 + array2))]
 
 
 if __name__ == '__main__':
-    print(combine_lists([1,2,3,4],[1,2,8,9,9]))
+    print(combine_lists([1, 2, 3, 4], [1, 2, 8, 9, 9]))
