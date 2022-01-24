216e478dc380b4d90d51b655c5cbb914984cefb2
tdxa <anndoman3@gmail.com>
2022-01-24 21:04:15 +0100

    Add cv2 figures

--- src/detector.py
+++ src/detector.py
@@ -1,24 +1,65 @@
 import cv2
 import numpy as np
-import imutils
+from cv2 import HOGDescriptor
 from imutils.object_detection import non_max_suppression
+from imutils.video import FPS
+
+from src.utils import FONT, FONT_COLOR, BOX_COLOR, get_text_size, FONT_SCALE, FONT_THICKNESS, DETAILS_COLOR, \
+    FONT_COLOR_SECONDARY
+
 
 class Detector:
     def __init__(self):
-        pass
+        self._available_methods = {'HOG': self.hog_detect}
 
     def load_img(self, path: str):
         img = cv2.imread(path)
         self._image = img
+        self._height = img.shape[0]
         self._width = img.shape[1]
 
     def show_image(self):
         cv2.imshow("Detection", self._image)
 
-    def image_detect(self, path: str):
+    def hog_detect(self):
+        descriptor = HOGDescriptor()
+        descriptor.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+        persons, weights = descriptor.detectMultiScale(self._image, winStride=(4, 4), padding=(8, 8), scale=1.03)
+
+        persons = np.array([[x, y, x + w, y + h] for (x, y, w, h) in persons])
+        persons = non_max_suppression(persons, probs=None, overlapThresh=0.5)
+
+        for index, (xA, yA, xB, yB) in enumerate(persons, start=1):
+            cv2.rectangle(self._image, (xA, yA), (xB, yB), BOX_COLOR, 2)
+            cv2.rectangle(self._image, (xA, yA - 20), (xB, yA), BOX_COLOR, -1)
+            cv2.putText(self._image, f'Person-{index}', (xA, yA), FONT, 0.5, FONT_COLOR, )
+
+        self._number_of_people = len(persons)
+
+    def draw_details(self, time):
+        text_width, text_height = get_text_size(f' Total: {self._number_of_people} Time: {time}'
+                                                "", FONT_SCALE, FONT_THICKNESS)
+
+        cv2.rectangle(self._image, (0, self._height), (self._width, self._height-text_height-15), DETAILS_COLOR, -1)
+
+        cv2.putText(self._image, f' Total: {self._number_of_people} Time: {time}', (10, self._height - 10), FONT,
+                    FONT_SCALE, FONT_COLOR_SECONDARY, FONT_THICKNESS)
+
+    def choose_method(self):
+        self._available_methods.get(self._model)()
+
+    def check_image(self, path: str, model: any):
         self.load_img(path)
+        self._model = model
+
+        time = FPS().start()
+        self.choose_method()
+        time.stop()
+
+        self.draw_details(time.elapsed())
         self.show_image()
         cv2.waitKey()
 
-    def video_detect(self):
+    def check_video(self):
         pass
---
+++ src/utils.py
@@ -0,0 +1,16 @@
+from cv2 import HOGDescriptor, FONT_HERSHEY_DUPLEX, getTextSize
+
+HOG = HOGDescriptor()
+
+BOX_COLOR = (0, 255, 2)
+DETAILS_COLOR = (0, 0, 0)
+FONT = FONT_HERSHEY_DUPLEX
+FONT_COLOR = (0, 0, 0)
+FONT_COLOR_SECONDARY = (255, 255, 255)
+FONT_SCALE = 0.5
+FONT_THICKNESS = 1
+
+
+def get_text_size(text, font_scale, font_thickness):
+    size, _ = getTextSize(text, FONT, font_scale, font_thickness)
+    return size
